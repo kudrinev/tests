e9a38e0f819d55dc7517310ef59a475962ca1eae
kudrinev <chesterdelauren@gmail.com>
2024-02-26 15:06:07 +0300

    add get posts/1

--- src/test/java/api/GetPostsTest.java
+++ src/test/java/api/GetPostsTest.java
@@ -2,18 +2,13 @@ package api;
 
 import api.response.GetPostsResponseItem;
 import io.restassured.http.ContentType;
-import io.restassured.internal.common.assertion.Assertion;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import api.response.GetPostsResponse;
-import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.post;
 
 public class GetPostsTest extends BaseApiTest {
 
@@ -31,7 +26,6 @@ public class GetPostsTest extends BaseApiTest {
             Assertions.assertEquals(postList.get(i).getId(), i + 1);
         }
     }
-
     @Test
     @DisplayName("GET /posts/1.Код 200.Проверка корректности ответа")
     public void getPostById() {
