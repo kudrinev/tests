cb733ff6263c153e18adb048b3a2b623ca28172d
kudrinev <chesterdelauren@gmail.com>
2024-02-27 09:44:33 +0300

    add tableTest

---
+++ src/test/java/api/api_test/TableTest.java
@@ -0,0 +1,48 @@
+package api.api_test;
+
+import api.response.getPostCommentsResponse.GetPostCommentsResponseItem;
+import api.response.getPostsResponse.GetPostsResponseItem;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class TableTest extends BaseApiTest {
+
+    @Test
+    @DisplayName("Вывод постов со связанными комментариями в таблицу")
+    public void getPostToCommentRelation() {
+        int postId;
+        setSpecification(requestSpecification(), response200Specification());
+
+        List<GetPostsResponseItem> postResponse = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("posts")
+                .then()
+                .extract().body().jsonPath().getList(".", GetPostsResponseItem.class);
+
+        System.out.printf("%-80s %-100s", "Пост", "Комментарий");
+        System.out.println();
+        for (int i = 0; i < postResponse.size(); i++) {
+            postId = i + 1;
+
+            List<GetPostCommentsResponseItem> commentList = given()
+                    .contentType(ContentType.JSON)
+                    .when()
+                    .get("posts/" + postId + "/comments")
+                    .then()
+                    .extract().body().jsonPath().getList(".", GetPostCommentsResponseItem.class);
+            System.out.printf("%-80s %-100s", postResponse.get(i).getTitle(), " ");
+
+            for (int j = 0; j < commentList.size(); j++) {
+                System.out.printf("%-80s %-100s", " ", commentList.get(j).getBody().replace("\n","") );
+                System.out.println();
+            }
+        }
+
+    }
+}
